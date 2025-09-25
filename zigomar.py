2f12b0c32f26c5f96565d307a67f9ab8b7b57559
Isaac Chrétien <2049728@cegepoutaouais.qc.ca>
2025-09-25 16:55:52 -0400

    Chasse au trésor Zigomar

---
+++ zigomar.py
@@ -0,0 +1,40 @@
+"""
+
+Fonctions :
+    1) import random : Permet d’utiliser les fonctions liées aux nombres aléatoires
+    2) random.shuffle(liste) : Mélange aléatoirement les éléments d’une liste ---> Exemple : random.shuffle(sac) → la liste sac est mélangée
+    3) random.choice(liste) : Retourne un élément aléatoire de la liste ---> Exemple : random.choice(sac) → choisit un objet au hasard dans sac
+
+Objectif :
+
+    1) Listes.
+
+    2) Boucles.
+
+    3) Fonctions.
+
+    4) Chaînes de caractères.
+
+TODO :
+
+    A) Organiser le travail en modules
+    B) Partager le projet sur GitHub avec l'équipe et l'enseignante
+
+
+Équipier : Isaac Chrétien
+
+
+"""
+
+sac {15} = "inventaire"
+potions {0, 15} = "potions scintillantes"
+cles {0, 15} = "clés mystérieuses"
+boules {0, 15} = "boules brillantes"
+squelettes {0, 15} = "squelettes miniatures"
+
+for i in range sac
+{0, 15}
+
+sac = {15}
+
+print()
